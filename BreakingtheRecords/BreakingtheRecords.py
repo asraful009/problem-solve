8446a098916c0cb1e57f3a5c7f7af82cb5b62495
Asraful Islam <asraful009@gmail.com>
2020-06-13 11:37:43 +0600

    done

--- BreakingtheRecords/BreakingtheRecords.py
+++ BreakingtheRecords/BreakingtheRecords.py
@@ -8,19 +8,27 @@ import sys
 
 # Complete the breakingRecords function below.
 def breakingRecords(scores):
-    return [1]
+    vMax = scores[0]
+    vMin = scores[0]
+    cMax = 0
+    cMin = 0
+    for i in scores:
+        if vMax < i:
+            cMax += 1
+            vMax = i
+        if vMin > i:
+            cMin += 1
+            vMin = i
+        #print("{} [{}:{}]/[{}:{}]".format(i, vMax, cMax, vMin, cMin))
+    #print("{} {}".format(cMax, cMin))
+    return [cMax, cMin]
 
 if __name__ == '__main__':
-    fptr = open(fptr = open("./output/my.txt", 'w'))
+    fptr = open("./output/my.txt", 'w')
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input())
-
     scores = list(map(int, input().rstrip().split()))
-
     result = breakingRecords(scores)
-
     fptr.write(' '.join(map(str, result)))
     fptr.write('\n')
-
     fptr.close()
