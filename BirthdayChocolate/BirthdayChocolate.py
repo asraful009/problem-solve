958f68acebe3b81a287a7c6b037e28cafe59aa14
Asraful Islam <asraful009@gmail.com>
2020-06-19 12:17:49 +0600

    Birthday Chocolate done

--- BirthdayChocolate/BirthdayChocolate.py
+++ BirthdayChocolate/BirthdayChocolate.py
@@ -8,19 +8,26 @@ import sys
 
 # Complete the birthday function below.
 def birthday(s, d, m):
+    totalCount = 0
     l = len(s)
+    # print("{} {} {}".format(s, d, m))
     for i in range(l):
         sv = 0
-        for j in range(m):
-            print("{} {} {} {}".format(sv, i, j, s[i + j]))
-            if l < j:
+        for j in range(i, i+m):
+            if(l == j):
                 break
-            sv += s[i + j]
-    return 1
+            # print("sv[{}, {}] : {} > {} >> {}".format(i, j, sv, s[j], abs(j-i+1)))
+            sv += s[j]
+            if sv == d and abs(j-i+1) == m:
+                totalCount += 1
+                # print("find => sv[{}, {}] : {} -> {}".format(i, j, sv, totalCount))
+                break
+    # print("count :{}".format(totalCount))
+    return totalCount
 
 if __name__ == '__main__':
-    fptr = open("./output/my.txt", 'w')
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # fptr = open("./output/my.txt", 'w')
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input().strip())
 
