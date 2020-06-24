39bb262e81ab0086d2ca51e9f31e66d0f4ba7efa
Asraful Islam <asraful009@gmail.com>
2020-06-24 23:00:35 +0600

    Divisible Sum Pairs done

--- DivisibleSumPairs/DivisibleSumPairs.py
+++ DivisibleSumPairs/DivisibleSumPairs.py
@@ -8,9 +8,19 @@ import sys
 
 # Complete the divisibleSumPairs function below.
 def divisibleSumPairs(n, k, ar):
-    pass
+    # print("{} {} {}".format(n, k, ar))
+    l = len(ar)
+    t = 0
+    for i in range(0, l):
+        for j in range(i+1, l):
+            s = ar[i]+ar[j]
+            if s%k == 0:
+                t+=1
+            # print("( ar[{}] = {} , ar[{}] = {} ) => {} -> {} >> {}".format(i, ar[i], j, ar[j], s, (s%k), t ))
+    return t
 
 if __name__ == '__main__':
+    # fptr = open("./output/my.txt", 'w')
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     nk = input().split()
