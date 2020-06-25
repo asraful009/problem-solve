0161a3669dc522ac67383b02abc9813670d6c98f
Asraful Islam <asraful009@gmail.com>
2020-06-25 23:15:46 +0600

    dayOfProgrammer init

---
+++ dayOfProgrammer/dayOfProgrammer.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the dayOfProgrammer function below.
+def dayOfProgrammer(year):
+    pass
+if __name__ == '__main__':
+        # fptr = open("./output/my.txt", 'w')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
