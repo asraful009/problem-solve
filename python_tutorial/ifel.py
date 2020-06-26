451318973d8a752ed5c6ef4ab661508ae5fa148b
Asraful Islam <asraful009@gmail.com>
2020-06-26 11:40:52 +0600

    python tutorial 0

---
+++ python_tutorial/ifel.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    If  n % 2 == 1 :
+        print("Weird")
+    If n % 2 == 0 and (n < 2 and n <5):
+        print("Not Weird")
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
