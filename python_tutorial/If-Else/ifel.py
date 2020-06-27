6eda8cb2d0f3cf10f7c63ffeabb835a9c55e1413
Asraful Islam <asraful009@gmail.com>
2020-06-27 10:21:16 +0600

    submit

--- python_tutorial/If-Else/ifel.py
+++ python_tutorial/If-Else/ifel.py
@@ -10,9 +10,11 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
-    If  n % 2 == 1 :
+    if n % 2 == 1:
         print("Weird")
-    If n % 2 == 0 and (n < 2 and n <5):
+    elif n % 2 == 0 and (n >= 2 and n <= 5):
         print("Not Weird")
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
+    elif n % 2 == 0 and (n >= 6 and n <= 20):
+        print("Weird")
+    else:
+        print("Not Weird")
