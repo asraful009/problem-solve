7e80accb4a9c92fadb06b134475b0f3513e12670
asraful islam <asraful009@gmail.com>
2022-08-21 21:56:06 +0600

    58

--- leetcode/35SearchInsertPosition/35SearchInsertPosition.java
+++ leetcode/35SearchInsertPosition/35SearchInsertPosition.java
@@ -1,14 +1,29 @@
 class Solution {
   public int searchInsert(int[] nums, int target) {
-    int pos = 0;
-    int mid = nums.length / 2;
 
-    return pos;
+    int s = 0;
+    int e = nums.length - 1;
+    int ret = -1;
+    int mid = 0;
+    while (s <= e) {
+      mid = s + (e - s) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      }
+
+      if (nums[mid] > target) {
+        e = mid - 1;
+      } else {
+        ret = mid;
+        s = mid + 1;
+      }
+    }
+    return ret + 1;
   }
 
   public static void main(String[] args) {
-    int[] nums = new int[] {};
-    int target = 4;
+    int[] nums = new int[] { 1, 3, 5, 6 };
+    int target = 5;
     var s = new Solution();
     System.out.println(s.searchInsert(nums, target));
   }
