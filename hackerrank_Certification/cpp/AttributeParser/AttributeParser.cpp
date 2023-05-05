30702584a6d13114bbd1ce7a82a8249271f8c20e
asraful islam <asraful009@gmail.com>
2023-05-05 20:43:11 +0600

    map

--- hackerrank_Certification/cpp/AttributeParser/AttributeParser.cpp
+++ hackerrank_Certification/cpp/AttributeParser/AttributeParser.cpp
@@ -4,7 +4,35 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <map>
+#include <regex>
 using namespace std;
+
+class HtmlObj
+{
+public:
+  string tag;
+  map<string, string> attributes;
+  vector<HtmlObj> childTags;
+  HtmlObj(const string tag, const map<string, string> attributes)
+  {
+    this->tag = tag;
+    this->attributes = attributes;
+  }
+};
+
+void splitAttributes(const string &attributes, map<string, string> &attributeMap)
+{
+  std::regex regex("\\s*([^=\\s]+)\\s*=\\s*\"([^\"]*)\"\\s*");
+  std::smatch match;
+
+  std::string::const_iterator searchStart(attributes.cbegin());
+  while (std::regex_search(searchStart, attributes.cend(), match, regex))
+  {
+    attributeMap[match[1].str()] = match[2].str();
+    searchStart = match.suffix().first;
+  }
+}
 
 void getInput(int lineCount, vector<string> &ref)
 {
@@ -18,6 +46,7 @@ void getInput(int lineCount, vector<string> &ref)
   //   cout << *it << '\n';
   // cout << endl;
 }
+
 vector<string> split(const string &input, char delimiter)
 {
   vector<std::string> result;
@@ -31,10 +60,41 @@ vector<string> split(const string &input, char delimiter)
 
   return result;
 }
-void htmlParsing(const string &htmlLine)
+
+vector<string> findTagAndAttributes(const string &rowLine)
 {
+  vector<string> tagAndAttr;
+  size_t pos = rowLine.find(' ');
+  if (pos != string::npos)
+  {
+    tagAndAttr.push_back(rowLine.substr(0, pos));
+    tagAndAttr.push_back(rowLine.substr(pos));
+  }
+  else
+  {
+    tagAndAttr.push_back(rowLine);
+  }
+  return tagAndAttr;
+}
+
+void htmlParser(const int index, const vector<string> &htmls, map<string, HtmlObj *> &mapHtml)
+{
+  string htmlLine = htmls[index];
   string rowLine = htmlLine.substr(1, htmlLine.length() - 2);
-  cout << rowLine << "\n";
+  cout << "[" << index << "] line: " << rowLine << "\n";
+  vector<string> tagAndAttrs = findTagAndAttributes(rowLine);
+  string tag = tagAndAttrs[0];
+  map<string, string> attributeMap;
+  if (tagAndAttrs.size() == 2)
+  {
+    splitAttributes(tagAndAttrs[1], attributeMap);
+  };
+  mapHtml["df"] = new HtmlObj(tag, attributeMap);
+
+  if (htmls.size() > index + 1)
+  {
+    htmlParser(index + 1, htmls, mapHtml);
+  }
 }
 
 int main()
@@ -46,11 +106,7 @@ int main()
   cin.ignore();
   getInput(htmlCount, htmls);
   getInput(queryCount, queries);
-
-  for (vector<string>::iterator it = htmls.begin(); it != htmls.end(); it++)
-  {
-    htmlParsing(*it);
-  }
-
+  map<string, HtmlObj *> mapHtml;
+  htmlParser(0, htmls, mapHtml);
   return 0;
 }
